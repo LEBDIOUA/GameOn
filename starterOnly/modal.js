b558c9e69170a798820e17cbeb1e3f274a59ff82
Amal LEBDIOUA DEMERY <lebdioua.amal@gmail.com>
2023-11-14 21:44:09 +0100

    Modification

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,8 @@ const modalClose = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const conteneur = document.querySelector(".content");
+const modalBody = document.querySelector(".modal-body");
+const myForm = document.querySelector("form");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,103 +23,166 @@ modalClose.addEventListener("click", fermerModal);
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  //Désactiver les messages de validation
+  if(!myForm.getAttribute("novalidate")){
+    myForm.setAttribute("novalidate", "")
+  }
 }
-
 // Fermer modal form
 function fermerModal() {
   modalbg.style.display = "none";
 }
-
 //Confirmation des données
-function confirmDonnees(){
-
+function confirmDonnees(event){
   let submit = true;
   formData.forEach((formInput) => {
     const inputElement = formInput.querySelector("input");
+    //Supprimer tous les msgs d'erreurs
     initialiserForm(formInput, 'p');
 
     // Récupérez et vérifier la valeur de l'élément input
     let detectErreur = validation(inputElement); 
     if (detectErreur[0]){
-
-      const pElement = document.createElement("p");
-      pElement.textContent = detectErreur[1];
-      pElement.style.color= "#FF4E60";
-      pElement.style.fontSize = "14px";
-      formInput.appendChild(pElement);
-
-      inputElement.style.border = "2px solid #FF4E60";
-      if (inputElement.type == "checkbox" && inputElement.id === "checkbox1"){
-        formInput.querySelector(".checkbox-icon").style.border= "2px solid #FF4E60";
-      }
-
+      //Créer une balise p et l'insérer pour afficher msg d'erreur 
+      creerElement(formInput, "p", "msgErreur", detectErreur[1]);
+      alerterChamp(inputElement, formInput);
       submit = false;
     }
   });
   if(submit == true){
-    initialiserForm(formData);
-
-    //message à afficher
-    const pElement = document.querySelector(".text-label");
-    pElement.textContent = "Merci pour votre inscription";
-    pElement.style.padding= "50% 0";
-    pElement.style.margin= "auto";
-    pElement.style.fontSize = "36px";
-    pElement.style.width = "271px";
-    pElement.style.textAlign= "center";
-
-    //boutton fermer
-    const btnElement = document.querySelector(".btn-submit");
-    btnElement.value = "Fermer";
-    btnElement.addEventListener("click", fermerModal);
+    initialiserForm(conteneur, modalBody);
+    creerElement(conteneur, "p", "msgFinal","Merci pour votre inscription");
+    creerElement(conteneur, "input", "btn-submit btn-fermer", "Fermer", "button")
+    conteneur.querySelector("input[type=button]").addEventListener("click", fermerModal)
   }
   return false;
 }
+function creerElement(conteneur, element, classe, contenue, type) {
+  const pElement = document.createElement(element);
+  if(type === "button"){
+    pElement.type = type;
+    afficherMsg(pElement, contenue, "value");
+  }
+  else if(element === "p"){
+    afficherMsg(pElement, contenue, "textContent");
+  }
+  if(classe !== ""){
+    pElement.className = classe;
+  }
+  conteneur.appendChild(pElement);
+}
+function supprimerElement(conteneur, element){
+  conteneur.removeChild(element);
+}
+function alerterChamp(champ, formInput){
+  champ.style.border = "2px solid #FF4E60";
+  if (champ.type == "checkbox" && champ.id === "checkbox1"){
+    formInput.querySelector(".checkbox-icon").style.border= "2px solid #FF4E60";
+  }
+}
+function afficherMsg(element, msg, attribut){
+  if(attribut === "textContent"){
+    element.textContent = msg;
+  }
+  else if(attribut === "value"){
+    element.value = msg;
+  }  
+}
+function initialiserForm(container, element){
+  if(element == 'p'){
+    // Parcourez les éléments p et supprimez-les
+    const pElements = container.querySelectorAll(".msgErreur");
+    if(pElements.length>0){
+      pElements.forEach((p) => {
+        supprimerElement(container, p);
+      });
 
-function validation(element){
-  let msg = [false, "msg"];
-  let listInput = ["text", "date", "email", "number"];
-
-  if (listInput.includes(element.type)){
-    if (element.value === ""){
-      msg[0] = true;
-      msg[1] = "Vous devez fournir une valeur";
-    }
-    else if (element.name == "first" || element.name == "last"){
-      if(element.value.length<2){
-        msg[0] = true;
-        msg[1] = "la valeur que vous avez fournie est érronée";
-      }
-    }
-    else if(element.type == "email"){
-      //Ce n'est pas nécessaire
-      if(!verifierEmail(element.value)){
-        msg[0] = true;
-        msg[1] = "la valeur que vous avez fournie est érronée";
-      }
-    }
-    else if(element.type == "date"){
-      //Ce n'est pas demandé, avoir au moin 15ans
-      const date = new Date();
-      let dateAnniv = new Date(element.value);
-      if((dateAnniv.getFullYear() + 15) > date.getFullYear()){
-        msg[0] = true;
-        msg[1] = "la valeur que vous avez fournie est érronée";
+      container.querySelector("input").style.border = "0";
+      if (container.querySelector("input").type == "checkbox" && container.querySelector("input").id === "checkbox1"){
+        container.querySelector(".checkbox-icon").style.border= "0";
       }
     }
   }
-  else if(element.type === "radio"){
+  else{
+    supprimerElement(container, element);
+  }
+}
+function validation(element){
+  let msg = [false, ""];
+  let listInput = ["text", "date", "email", "number"];
+  if (listInput.includes(element.type)){
+    if (element.name == "first" || element.name == "last"){
+      msg = verifierText(element);
+    }
+    else if(element.name == "quantity"){
+      msg = verifierEntier(element);
+    }
+    else if(element.type == "email"){
+      msg = verifierEmail(element);
+    }
+    else if(element.type == "date"){
+      msg = verifierDate(element);
+    }
+  }
+  else if(element.type === "radio" || element.type === "checkbox"){
+    msg = verifierCheck(element);
+  }
+  return msg;
+}
+function verifierText(element){
+  let msg = [false, ""];
+  if (element.value === ""){
+    msg[0] = true;
+    msg[1] = "Veuillez fournir une valeur";
+  }
+  else if(element.value.length<2){
+    msg[0] = true;
+    msg[1] = "Veuillez allonger ce texte pour qu'il comporte au moins 2 caractères. il en compte acctuellement un seul";
+  }
+  return msg;
+}
+function verifierDate(element){
+  let msg = [false, ""];
+  if (element.value === ""){
+    msg[0] = true;
+    msg[1] = "Veuillez fournir une valeur";
+  }
+  else{
+    //Ce n'est pas demandé, avoir au moin 15ans
+    const date = new Date();
+    let dateAnniv = new Date(element.value);
+    if((dateAnniv.getFullYear() + 15) > date.getFullYear()){
+      msg[0] = true;
+      msg[1] = "Vous devez avoir au moins 15ans pour participer";
+    }
+  }
+  return msg;
+}
+function verifierEmail(element){
+  let msg = [false, ""];
+  let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (element.value === ""){
+    msg[0] = true;
+    msg[1] = "Veuillez fournir une valeur";
+  }
+  else if(!element.value.match(emailFormat)){
+    msg[0] = true;
+    msg[1] = "La valeur que vous avez fournie est érronée";
+  }
+  return msg; 
+}
+function verifierCheck(element){
+  let msg = [false, ""];
+  if(element.type === "radio"){
     const tournois = element.form.querySelectorAll('input[type="radio"]');
     let i=0;
     let tournoisChecked = false;
-
     do{
       if(tournois[i].checked){
         tournoisChecked = true;
       }
       i++;
     }while(i<tournois.length && !tournoisChecked);
-
     if (tournoisChecked == false){
       msg[0] = true;
       msg[1] = "Aucun tournois selectioné";
@@ -129,39 +194,17 @@ function validation(element){
       msg[1] = "Veuillez accepter les conditions";
     }
   }
-
   return msg;
 }
-
-function initialiserForm(formInput, element){
-  if(element == 'p'){
-    // Parcourez les éléments p et supprimez-les
-    const pElements = formInput.querySelectorAll(element);
-    if(pElements.length>0){
-      pElements.forEach((p) => {
-        formInput.removeChild(p);
-      });
-
-      formInput.querySelector("input").style.border = "0";
-      if (formInput.querySelector("input").type == "checkbox" && formInput.querySelector("input").id === "checkbox1"){
-        formInput.querySelector(".checkbox-icon").style.border= "0";
-      }
-    }
+function verifierEntier(element){
+  let msg = [false, ""];
+  if (element.value === ""){
+    msg[0] = true;
+    msg[1] = "Veuillez fournir une valeur";
   }
-  else{
-    formInput.forEach((formElement) => {
-      formElement.remove();
-    });    
-    // document.querySelector(".text-label").remove();
-    // document.querySelector(".btn-submit").remove();
+  else if(element.value < 0){
+    msg[0] = true;
+    msg[1] = "Veuillez saisir un nombre positif";
   }
-}
-function verifierEmail(element){
-  let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(element.match(emailFormat)){
-    return true;
-  }
-  else{
-    return false;
-  } 
+  return msg;
 }
